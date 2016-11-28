eed9db4ab236f9a1505485d58727a848eb049a3d
Thomas Miller <thomasmillergb@outlook.com>
2016-11-28 21:17:34 +0000

    Initial commit

---
+++ src/main/java/com/thomasmillergb/ThomasMingyiWeddingApplication.java
@@ -0,0 +1,12 @@
+package com.thomasmillergb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThomasMingyiWeddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThomasMingyiWeddingApplication.class, args);
+	}
+}
